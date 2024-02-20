1c7ce2f0cfb80bcee346de006d2911fc4f8e1ee3
Anatolii Chuldum <tajkinol@gmail.com>
2024-02-20 14:50:41 +0700

    2 commit

--- proba1.cpp
+++ proba1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"uiiouiou", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg {"Проба 2", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
     for (const string& word : msg)
     {
