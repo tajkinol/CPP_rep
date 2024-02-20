5f5124db40dbf17cedb708c99257ef00da5b9989
Anatolii Chuldum <tajkinol@gmail.com>
2024-02-20 15:45:10 +0700

    fix 6 change

--- proba1.cpp
+++ proba1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-    vector<string> msg {"Проба 4", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    vector<string> msg {"Проба 575765", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
     for (const string& word : msg)
     {
